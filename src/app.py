60b1a71f68ca564697a144b65c894dd2850a9824
d-Narwani <65844711+d-Narwani@users.noreply.github.com>
2020-06-28 00:36:50 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ def create_app(test_config=None):
   CORS(app, resources={r"/*": {"origins": "*"}})
   setup_db(app)
 
-  db_drop_and_create_all()
+  #db_drop_and_create_all()
   
   @app.after_request
   def after_request(response):
