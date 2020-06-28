08187c75e1f5848165e39aacfa78f9fb353515b3
d-Narwani <65844711+d-Narwani@users.noreply.github.com>
2020-06-28 14:37:35 +0530

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
 from datetime import *
 from datetime import date
 import os
@@ -7,6 +6,7 @@ import os
 database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
+
 
 def setup_db(app):
     """binds a flask application and a SQLAlchemy service"""
@@ -16,6 +16,7 @@ def setup_db(app):
     db.init_app(app)
     db.create_all()
 
+
 def db_drop_and_create_all():
     '''drops the database tables and starts fresh
     can be used to initialize a clean database
@@ -24,34 +25,35 @@ def db_drop_and_create_all():
     db.create_all()
     db_init_records()
 
+
 def db_init_records():
     '''this will initialize the database with some test records.'''
 
     new_actor = (Actor(
-        name = 'Dhaval',
-        gender = 'Male',
-        age = 25
-        ))
+        name='Dhaval',
+        gender='Male',
+        age=25
+    ))
 
     new_movie = (Movie(
-        title = 'Dhaval first Movie',
-        year = 2020,
-        director = 'Dabra'
-        ))
+        title='Dhaval first Movie',
+        year=2020,
+        director='Dabra'
+    ))
 
     new_actor.insert()
     new_movie.insert()
     db.session.commit()
 
 
-class  Actor(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
+class Actor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     gender = db.Column(db.String(20), nullable=False)
     age = db.Column(db.Integer)
 
     def __repr__(self):
-        return f"Actor id:'{self.id}',name='{self.name}'"
+        return f"Actor id:'{self.id}', name='{self.name}'"
 
     def insert(self):
         db.session.add(self)
@@ -66,21 +68,21 @@ class  Actor(db.Model):
 
     def to_dict(self):
         return {
-            'id':self.id,
-            'name':self.name,
-            'gender':self.gender,
-            'age':self.age
+            'id': self.id,
+            'name': self.name,
+            'gender': self.gender,
+            'age': self.age
         }
 
 
 class Movie(db.Model):
-    id = id = db.Column(db.Integer,primary_key=True)
+    id = id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     year = db.Column(db.Integer)
-    director = db.Column(db.String(100),nullable=False)
+    director = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return f"Movie id:'{self.id}',title:'{self.title}'"
+        return f"Movie id:'{self.id}', title:'{self.title}'"
 
     def insert(self):
         db.session.add(self)
@@ -95,10 +97,8 @@ class Movie(db.Model):
 
     def to_dict(self):
         return {
-            'id':self.id,
-            'title':self.title,
-            'director':self.director,
-            'year':self.year
+            'id': self.id,
+            'title': self.title,
+            'director': self.director,
+            'year': self.year
         }
-
-
